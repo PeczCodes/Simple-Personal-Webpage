28fd1a6e11fffd215345c76bfd017988479bcde3
pecz-codes <peculiaramile@gmail.com>
2025-04-16 21:56:55 +0100

    hamburger menu and other javascript interactions have been added

---
+++ index.js
@@ -0,0 +1,11 @@
+const contactButton = document.querySelector(".contact");
+const hamburger = document.querySelector(".hamburger");
+const hamburgerDiv = document.querySelector(".hamburger").querySelectorAll("div");
+const hamburgerNav = document.querySelector(".hamburger-menu");
+
+contactButton.addEventListener("click", (e) => console.log("clicked"));
+
+hamburger.addEventListener("click", (e) => {
+	hamburgerNav.classList.toggle("active");
+	hamburgerDiv.forEach(div => div.classList.toggle("active"));
+});
